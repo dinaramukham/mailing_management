75278a9f2d9821a8b5b12f690aea78c2e6216f31
Dinara Mukhametzianova <muxametmdm@gmail.com>
2024-01-19 23:00:16 +0500

    права доступа

--- mailing_management/mailing/views.py
+++ mailing_management/mailing/views.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.core.mail import send_mail
 from django.http import Http404
 from django.shortcuts import render
@@ -16,11 +16,13 @@ from mailing_management import settings
 class MailingListView(ListView):
     model = Mailing
 
-class MailingDetailView(DetailView):
+class MailingDetailView(PermissionRequiredMixin, DetailView):
+    permission_required = 'mailing.view_mailing'
     model= Mailing
 
-class MailingUpdateView(LoginRequiredMixin, UpdateView):
+class MailingUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     model=Mailing
+    permission_required = 'mailing.change_mailing'
     #fields=['client', 'log', 'letter', 'date_from', 'date_to', 'period', 'status']
     form_class = MailingForm
     success_url = reverse_lazy('mailing_list')
@@ -36,26 +38,30 @@ class MailingCreateView(LoginRequiredMixin, CreateView):
     success_url = reverse_lazy('mailing_list')
 
 
-class MailingDeleteView(LoginRequiredMixin, DeleteView):
+class MailingDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
     model=Mailing
+    permission_required = 'mailing.delete_mailing'
     success_url = reverse_lazy('mailing_list')
     def get_object(self, queryset=None):
         self.object = super().get_object(queryset)
         if self.object.user != self.request.user:
             raise Http404
         return self.object
-class LetterCreateView(LoginRequiredMixin, CreateView):
+class LetterCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     model=Letter
+    permission_required = 'mailing.add_letter'
     fields=['title','content']
     success_url = reverse_lazy('mailing_create')
-class ClientCreateView(LoginRequiredMixin, CreateView):
+class ClientCreateView(LoginRequiredMixin, PermissionRequiredMixin,CreateView):
     model =Client
+    permission_required = 'mailing.add_client'
     #fields = ['name', 'email', 'message']
     form_class = ClientForm
     success_url = reverse_lazy('mailing_list')
 
-class ClientUpdateView(LoginRequiredMixin, UpdateView):
+class ClientUpdateView(LoginRequiredMixin, PermissionRequiredMixin,UpdateView):
     model=Client
+    permission_required = 'mailing.change_letter'
     #fields=['name', 'email', 'message']
     form_class = ClientForm
     success_url = reverse_lazy('mailing_list')
@@ -66,14 +72,12 @@ class ClientUpdateView(LoginRequiredMixin, UpdateView):
         return self.object
 
 
-
-
-
 class ClientListView( ListView):
     model = Client
 
-class ClientDetailView(LoginRequiredMixin, DetailView):
+class ClientDetailView(LoginRequiredMixin, PermissionRequiredMixin,DetailView):
     model=Client
+    permission_required = 'mailing.view_letter'
 
 
 def exzample(request):
